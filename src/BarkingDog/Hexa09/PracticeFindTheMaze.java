1357332f3ccb1374cd4f57551e1e17063097c1ff
choyi918 <choyi918@naver.com>
2023-11-10 18:32:20 +0900

    다시 풀이

--- src/BarkingDog/Hexa09/PracticeFindTheMaze.java
+++ src/BarkingDog/Hexa09/PracticeFindTheMaze.java
@@ -3,9 +3,75 @@ package BarkingDog.Hexa09;
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class PracticeFindTheMaze {
     public static void main(String[] args) throws IOException {
+        // solve();
+        again();
+    }
+
+    private static void again() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        {
+            StringTokenizer spec = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(spec.nextToken());
+            int h = Integer.parseInt(spec.nextToken());
+
+            int[][] board = new int[v][h];
+            int[][] distance = new int[v][h];
+            for (int i = 0; i < v; i++) {
+                String[] tokens = br.readLine().split("");
+                for (int j = 0; j < h; j++) {
+                    board[i][j] = Integer.parseInt(tokens[j]);
+                    distance[i][j] = -1;
+                }
+            }
+
+            Queue<Pos> queue = new LinkedList<>();
+
+            queue.add(new Pos(0, 0));
+
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, -1, 0, 1};
+
+            while (queue.isEmpty() == false) {
+                Pos popped = queue.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = popped.x + dx[i];
+                    int ny = popped.y + dy[i];
+
+                    if (nx == v - 1 && ny == h - 1) {
+                        bw.write(String.valueOf(distance[popped.x][popped.y] + 1));
+                        bw.flush();
+                        return;
+                    }
+
+                    if (nx < 0 || nx >= v || ny <0 || ny >= h)
+                        continue;
+
+                    if (board[nx][ny] == 0 || distance[nx][ny] != -1)
+                        continue;
+
+                    distance[nx][ny] = distance[popped.x][popped.y] + 1;
+                    queue.add(new Pos(nx, ny));
+                }
+            }
+        }
+
+        isr.close();
+        osw.close();
+        br.close();
+        bw.close();
+    }
+
+    public static void solve() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
--- src/BarkingDog/Hexa09/PracticePicture.java
+++ src/BarkingDog/Hexa09/PracticePicture.java
@@ -3,12 +3,98 @@ package BarkingDog.Hexa09;
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 /*
  * https://www.acmicpc.net/problem/1926
  */
 public class PracticePicture {
     public static void main(String[] args) throws IOException {
+        // solve();
+        again();
+    }
+
+    private static void again() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+
+        int[][] board = new int[v][h];
+        boolean[][] visited = new boolean[v][h];
+
+        for (int i = 0; i < v; i++) {
+
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < h; j++) {
+                board[i][j] = Integer.parseInt(st2.nextToken());
+            }
+        }
+
+        Queue<Pos> queue = new LinkedList<Pos>();
+
+        int pictureCount = 0;
+        int largestArea = 0;
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < h; j++) {
+                if (board[i][j] == 0 || visited[i][j] == true)
+                    continue;
+
+                int area = 0;
+
+                if (board[i][j] == 1) {
+                    visited[i][j] = true;
+                    area++;
+                    queue.add(new Pos(i, j));
+                    pictureCount++;
+                }
+
+                
+                while (queue.isEmpty() == false) {
+                    Pos popped = queue.poll();
+
+                    for (int k = 0; k < 4; k++) {
+                        int nx = popped.x + dx[k];
+                        int ny = popped.y + dy[k];
+
+                        if (nx < 0 || nx >= v || ny < 0 || ny >= h)
+                            continue;
+
+                        if (board[nx][ny] == 0 || visited[nx][ny] == true)
+                            continue;
+
+                        queue.add(new Pos(nx, ny));
+                        area++;
+                        visited[nx][ny] = true;
+                    }
+                }
+
+                if (largestArea < area)
+                    largestArea = area;
+            }
+        }
+
+        bw.write(String.valueOf(pictureCount) + "\n");
+        bw.write(String.valueOf(largestArea) + "\n");
+        bw.flush();
+
+        isr.close();
+        br.close();
+        osw.close();
+        bw.close();
+    }
+
+    private static void solve() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         OutputStreamWriter osw = new OutputStreamWriter(System.out);
@@ -117,9 +203,5 @@ public class PracticePicture {
             this.x = x;
             this.y = y;
         }
-    }
-
-    private static void solve(final int n, final int m) {
-
     }
 }
